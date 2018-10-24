66473c78a2470fe013932ba68edd580b6feff4c7
SongChunPeng <154518499@qq.com>
2018-10-24 17:21:25 +0800

    单例模式

--- src/singleton/TestSingleton.java
+++ src/singleton/TestSingleton.java
@@ -8,5 +8,6 @@ package singleton;
 public class TestSingleton {
     public static void main(String[] args) {
         System.out.println(SingletonDemo5.INSTANCE==SingletonDemo5.INSTANCE);
+        SingletonDemo5.INSTANCE.singletonOperation();
     }
 }
